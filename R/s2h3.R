d3fa0df64a4c3d6e75c6c9a531bfbea531023002
benyamindsmith <benyamindsmith@gmail.com>
2023-07-23 23:17:00 -0400

    first commit

---
+++ R/s2h3.R
@@ -0,0 +1,17 @@
+#' Get Corresponding S2 and H3 cells for given longitude and latitude
+#'
+#' This function is a work in progress!
+#'
+#' @param data The dataframe or tibble that contains a longitude and latitude field for individual points of interest
+#' @param long The longitude field name in `data`
+#' @param lat The latitude field name in `data`
+#' @param s2_level The desired [s2 level](https://s2geometry.io/resources/s2cell_statistics.html)
+#' @param h3_resolution The desired [h3 resolution](https://h3geo.org/docs/core-library/restable/)
+
+s2h3 <- function(data,
+                 long,
+                 lat,
+                 s2_level,
+                 h3_resolution){
+
+}
